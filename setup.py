857f34147ad1f75324336a86f9d52a748a48ce33
durban.zhang <durban.zhang@gmail.com>
2019-12-26 10:42:27 +0800

    update readme

--- setup.py
+++ setup.py
@@ -2,23 +2,22 @@
 # @Author: durban
 # @Date:   2019-12-25 18:00:43
 # @Last Modified by:   durban.zhang
-# @Last Modified time: 2019-12-26 10:35:54
+# @Last Modified time: 2019-12-26 10:41:47
 
 from setuptools import find_packages, setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.rst", "r") as fh:
+    readme = fh.read()
 
 setup(
     name='Flask-DXCaptcha',
-    version='1.0.2',
+    version='1.0.3',
     url='https://github.com/durban89/flask_dxcaptcha',
     license='MIT',
     author='durban zhang',
     author_email='durban.zhang@gmail.com',
     description='Flask-DXCaptcha是依赖顶象科技提供的无感验证功能开发的Flask 扩展',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description=readme,
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
