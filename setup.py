9bea6083aacc52cdd2f15741ef778fa2c0fe768d
durban.zhang <durban.zhang@gmail.com>
2019-12-26 09:50:29 +0800

    update setup

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
